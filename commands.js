0b23ae8abab9c21c4c2723bf4974c12ed2e20f2d
rickymarin <cartidge81@gmail.com>
2023-09-04 11:47:05 -0400

    updates

--- commands.js
+++ commands.js
@@ -1,45 +1,27 @@
 import 'dotenv/config';
-import { getRPSChoices } from './game.js';
-import { capitalize, InstallGlobalCommands } from './utils.js';
+import { InstallGlobalCommands } from './utils.js';
 
-// Get the game choices from game.js
-function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
 
-  for (let choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
-  }
-
-  return commandChoices;
-}
-
-// Simple test command
-const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
+const SET_CAP_COMMAND = {
+  name: 'set-cap',
+  description: 'Set number of people for event',
   type: 1,
 };
 
-// Command containing options
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
-  options: [
-    {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
-      required: true,
-      choices: createCommandChoices(),
-    },
-  ],
+const RESERVE_COMMAND = {
+  name: 'reserve',
+  description: 'Add yourself to the list',
   type: 1,
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+const WAITLIST_COMMAND = {
+  name: 'waitlist',
+  description: 'Add yourself to the waitlist',
+  type: 1,
+};
+
+
+
+const ALL_COMMANDS = [SET_CAP_COMMAND, RESERVE_COMMAND,WAITLIST_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
